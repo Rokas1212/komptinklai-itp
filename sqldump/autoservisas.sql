a7870d08caf18f08b81a2cb10660c5cc34e48a71
Rokas Ciuplinskas <r.ciuplinskas@gmail.com>
2024-11-20 00:39:53 +0200

    update dump

--- sqldump/autoservisas.sql
+++ sqldump/autoservisas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: db
--- Generation Time: Nov 03, 2024 at 04:05 PM
+-- Generation Time: Nov 19, 2024 at 10:39 PM
 -- Server version: 5.7.44
 -- PHP Version: 8.2.8
 
@@ -44,6 +44,18 @@ INSERT INTO `DUK` (`duk_id`, `klausimas`, `atsakymas`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `MeistrasPaslaugos`
+--
+
+CREATE TABLE `MeistrasPaslaugos` (
+  `id` int(11) NOT NULL,
+  `meistro_id` int(11) NOT NULL,
+  `paslaugos_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `Naudotojai`
 --
 
@@ -53,21 +65,23 @@ CREATE TABLE `Naudotojai` (
   `el_pastas` varchar(255) COLLATE utf8_lithuanian_ci NOT NULL,
   `slaptazodis` varchar(255) COLLATE utf8_lithuanian_ci NOT NULL,
   `telefono_numeris` varchar(20) COLLATE utf8_lithuanian_ci DEFAULT NULL,
-  `vaidmuo` enum('klientas','meistras','vadybininkas') COLLATE utf8_lithuanian_ci NOT NULL
+  `vaidmuo` enum('klientas','meistras','vadybininkas') COLLATE utf8_lithuanian_ci NOT NULL,
+  `aprasymas` text COLLATE utf8_lithuanian_ci NOT NULL,
+  `nuotrauka` text COLLATE utf8_lithuanian_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_lithuanian_ci;
 
 --
 -- Dumping data for table `Naudotojai`
 --
 
-INSERT INTO `Naudotojai` (`naudotojo_id`, `vardas`, `el_pastas`, `slaptazodis`, `telefono_numeris`, `vaidmuo`) VALUES
-(1, 'Jonas', 'jonas@example.com', '$2y$10$b3lI6q6gvEcczBJ249juV.y7cois8bcz.R2fjh6ocQ2STLizlPe2e', '+37060000000', 'klientas'),
-(2, 'Jonas', 'jonas1@example.com', '$2y$10$GH6gPO8/LUOlt3FTIYxcAeTzdHA3u16XqfL01jlYZYvexQKSDmAWS', '+37060000000', 'meistras'),
-(4, 'Petras', 'Petras@meistas.lt', 'Password123!', '+37065793412', 'meistras'),
-(13, 'Rokas', 'rokas@admin.com', '$2y$10$GH6gPO8/LUOlt3FTIYxcAeTzdHA3u16XqfL01jlYZYvexQKSDmAWS', '+37065750867', 'klientas'),
-(14, 'Vadybininkas', 'vadybininkas@administracija.lt', '$2y$10$CLY9E6z0UznDWz5gnmMfZ.rERdB7JhiT0APz1rcumICLZZu9QbdXa', '+37058137412', 'vadybininkas'),
-(15, 'Meistrelis', 'meistrelis@meistriukas.com', '$2y$10$RVQd0E9rC/F3yp/E3jFb9uZTuCeirKZ8XgLTBTimh1fqMgkGoRESW', '+35192857', 'meistras'),
-(16, 'Pranas', 'pranelis@gmail.com', '$2y$10$9mtLwKL9RagL3Fpn/s0V0uOvfqZr4/09GElYrbNdgY/IKCje2GjYm', '+37065969999', 'meistras');
+INSERT INTO `Naudotojai` (`naudotojo_id`, `vardas`, `el_pastas`, `slaptazodis`, `telefono_numeris`, `vaidmuo`, `aprasymas`, `nuotrauka`) VALUES
+(1, 'Jonas', 'jonas@example.com', '$2y$10$b3lI6q6gvEcczBJ249juV.y7cois8bcz.R2fjh6ocQ2STLizlPe2e', '+37060000000', 'klientas', '', ''),
+(2, 'Jonas', 'jonas1@example.com', '$2y$10$GH6gPO8/LUOlt3FTIYxcAeTzdHA3u16XqfL01jlYZYvexQKSDmAWS', '+37060000000', 'meistras', 'Pats geriausias meistrelis', 'https://www.shutterstock.com/image-photo/young-african-american-mechanic-working-600nw-2099017543.jpg'),
+(4, 'Petras', 'Petras@meistas.lt', 'Password123!', '+37065793412', 'meistras', '', 'https://www.shutterstock.com/shutterstock/photos/1711144648/display_1500/stock-photo-portrait-shot-of-a-handsome-mechanic-working-on-a-vehicle-in-a-car-service-professional-repairman-1711144648.jpg'),
+(13, 'Rokas', 'rokas@admin.com', '$2y$10$GH6gPO8/LUOlt3FTIYxcAeTzdHA3u16XqfL01jlYZYvexQKSDmAWS', '+37065750867', 'klientas', '', ''),
+(14, 'Vadybininkas', 'vadybininkas@administracija.lt', '$2y$10$CLY9E6z0UznDWz5gnmMfZ.rERdB7JhiT0APz1rcumICLZZu9QbdXa', '+37058137412', 'vadybininkas', '', ''),
+(15, 'Meistrelis', 'meistrelis@meistriukas.com', '$2y$10$RVQd0E9rC/F3yp/E3jFb9uZTuCeirKZ8XgLTBTimh1fqMgkGoRESW', '+35192857', 'meistras', '', ''),
+(16, 'Pranas', 'pranelis@gmail.com', '$2y$10$9mtLwKL9RagL3Fpn/s0V0uOvfqZr4/09GElYrbNdgY/IKCje2GjYm', '+37065969999', 'meistras', '', '');
 
 -- --------------------------------------------------------
 
@@ -129,6 +143,14 @@ CREATE TABLE `Ratings` (
   `rating` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_lithuanian_ci;
 
+--
+-- Dumping data for table `Ratings`
+--
+
+INSERT INTO `Ratings` (`rating_id`, `meistro_id`, `kliento_id`, `rating`) VALUES
+(1, 2, 14, 4),
+(2, 15, 14, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -157,8 +179,11 @@ INSERT INTO `Rezervacijos` (`rezervacijos_id`, `kliento_id`, `meistro_id`, `pasl
 (19, 2, 4, 1, '2024-11-22', '12:00:00'),
 (20, 2, 2, 1, '2024-11-16', '11:00:00'),
 (22, 2, 2, 1, '2024-11-16', '12:00:00'),
-(23, 14, 2, 2, '2024-11-17', '11:00:00'),
-(24, 2, 15, 2, '2024-11-08', '10:00:00');
+(23, 14, 2, 2, '2024-11-08', '11:00:00'),
+(24, 2, 15, 2, '2024-11-08', '10:00:00'),
+(25, 14, 2, 2, '2024-11-01', '11:00:00'),
+(27, 14, 2, 5, '2024-11-06', '09:00:00'),
+(28, 14, 15, 5, '2024-11-01', '11:00:00');
 
 --
 -- Indexes for dumped tables
@@ -169,6 +194,14 @@ INSERT INTO `Rezervacijos` (`rezervacijos_id`, `kliento_id`, `meistro_id`, `pasl
 --
 ALTER TABLE `DUK`
   ADD PRIMARY KEY (`duk_id`);
+
+--
+-- Indexes for table `MeistrasPaslaugos`
+--
+ALTER TABLE `MeistrasPaslaugos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `meistro_id` (`meistro_id`),
+  ADD KEY `paslaugos_id` (`paslaugos_id`);
 
 --
 -- Indexes for table `Naudotojai`
@@ -218,6 +251,12 @@ ALTER TABLE `DUK`
   MODIFY `duk_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
+-- AUTO_INCREMENT for table `MeistrasPaslaugos`
+--
+ALTER TABLE `MeistrasPaslaugos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `Naudotojai`
 --
 ALTER TABLE `Naudotojai`
@@ -239,17 +278,24 @@ ALTER TABLE `Prieinamumas`
 -- AUTO_INCREMENT for table `Ratings`
 --
 ALTER TABLE `Ratings`
-  MODIFY `rating_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `rating_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `Rezervacijos`
 --
 ALTER TABLE `Rezervacijos`
-  MODIFY `rezervacijos_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `rezervacijos_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `MeistrasPaslaugos`
+--
+ALTER TABLE `MeistrasPaslaugos`
+  ADD CONSTRAINT `meistro_id` FOREIGN KEY (`meistro_id`) REFERENCES `Naudotojai` (`naudotojo_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `paslaugos_id` FOREIGN KEY (`paslaugos_id`) REFERENCES `Paslaugos` (`paslaugos_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `Prieinamumas`
