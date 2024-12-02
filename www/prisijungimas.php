d81e5fc1da3f172ec08057122522309c9d9384f6
Rokas Čiuplinskas <r.ciuplinskas@gmail.com>
2024-12-02 23:09:19 +0200

    update prisijungimas

--- www/prisijungimas.php
+++ www/prisijungimas.php
@@ -9,7 +9,7 @@ if (isset($_SESSION['naudotojoId'])) {
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $el_pastas = $_POST['el_pastas'];
-    $slaptazodis = $_POST['slaptazodis'];
+    $prisijungimo_slaptazodis = $_POST['prisijungimo_slaptazodis'];
     
     $sql = "SELECT naudotojo_id, vardas, slaptazodis, vaidmuo FROM Naudotojai WHERE el_pastas = ?";
     $stmt = $mysqli->prepare($sql);
@@ -20,7 +20,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $stmt->bind_result($naudotojoId, $vardas, $hashedPassword, $vaidmuo);
     $stmt->fetch();
     
-    if ($stmt->num_rows == 1 && password_verify($slaptazodis, $hashedPassword)) {
+    if ($stmt->num_rows == 1 && password_verify($prisijungimo_slaptazodis, $hashedPassword)) {
         $_SESSION['naudotojoId'] = $naudotojoId;
         $_SESSION['vardas'] = $vardas;
         $_SESSION['vaidmuo'] = $vaidmuo;
@@ -40,11 +40,11 @@ include 'header.php';
     <form action="prisijungimas.php" method="post">
         <div class="form-group">
             <label for="el_pastas">el.paštas:</label>
-            <input type="text" class="form-control" id="el_pastas" name="el_pastas" required>
+            <input type="text" class="form-control" id="el_pastas" name="el_pastas" required value="<?php echo isset($el_pastas) ? $el_pastas : ''; ?>">
         </div>
         <div class="form-group">
-            <label for="slaptazodis">Slaptažodis:</label>
-            <input type="password" class="form-control" id="slaptazodis" name="slaptazodis" required>
+            <label for="prisijungimo_slaptazodis">Slaptažodis:</label>
+            <input type="password" class="form-control" id="prisijungimo_slaptazodis" name="prisijungimo_slaptazodis" required value="<?php echo isset($prisijungimo_slaptazodis) ? $prisijungimo_slaptazodis : '';?>">
         </div>
         <button type="submit" class="btn btn-primary">Prisijungti</button>
         <a href="registracija.php" class="btn btn-link">Registruotis</a>
